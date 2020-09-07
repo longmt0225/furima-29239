d46d60ba41dab77856ee90354dc6f4bff457ac8c
longmt0225 <longmt.k8.changshan21@gmail.com>
2020-09-07 17:40:41 +0900

    ユーザー管理機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # rescue_from Exception, with: :rescue_from_exception
+
+  protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(
+			:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,37 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many :items dependent: :destroy
+  # has_many :purchases dependent: :destroy
+
+  validates :nickname             , presence: true
+
+  validates :email                , presence: true, uniqueness: true
+  
+  validates :password             , presence: true,
+             format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,128}\z/,
+             message: "は半角6~128文字英小文字・数字それぞれ１文字以上含む必要があります"}
+
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  
+  validates :family_name          , presence: true, 
+             format: { with: /\A[ぁ-んァ-ン一-龥]/,
+             message: "は全角（漢字・ひらがな・カタカナ）で入力する必要があります"}
+  
+  validates :first_name          , presence: true, 
+             format: { with: /\A[ぁ-んァ-ン一-龥]/,
+             message: "は全角（漢字・ひらがな・カタカナ）で入力する必要があります"}
+
+  validates :family_name_kana    , presence: true, 
+             format: { with: /\A[ァ-ヶー－]+\z/,
+             message: "は全角（カタカナ）で入力する必要があります"}
+  
+  validates :first_name_kana     , presence: true, 
+             format: { with: /\A[ァ-ヶー－]+\z/,
+             message: "は全角（カタカナ）で入力する必要があります"}
+
+  validates :birth_date           , presence: true
+
+
 end
