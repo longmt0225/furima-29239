5d3feeec3843c3b3c02485be61b0641b42fd9927
longmt0225 <longmt.k8.changshan21@gmail.com>
2020-09-11 16:48:32 +0900

    商品出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @items = Item.all.order('id DESC')
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(
+      :name, :price, :description, :user, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :image)
+      .merge(user_id: current_user.id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,30 +4,41 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :items dependent: :destroy
-  # has_many :purchases dependent: :destroy
+  # Association
+  has_many :items, dependent: :destroy
+  has_many :purchases, dependent: :destroy
 
+  # Validation
   validates :nickname, presence: true
 
-  validates :password, # presence: true
-            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,128}\z/,
-                      message: 'includes both letters and numbers' }
+  validates :email, presence: true, uniqueness: true
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,128}\z/
+  validates :password, presence: true,
+                       format: { with: VALID_PASSWORD_REGEX,
+                                 message: 'includes both letters and numbers'}
+
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :family_name, presence: true,
-                          format: { with: /\A[ぁ-んァ-ン一-龥]/,
-                                    message: 'Full-width characters' }
+                          format: { with: NAME_REGEX,
+                                    message: 'Full-width characters'}
 
   validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ン一-龥]/,
-                                   message: 'Full-width characters' }
+                         format: { with: NAME_REGEX,
+                                   message: 'Full-width characters'}
 
   validates :family_name_kana, presence: true,
-                               format: { with: /\A[ァ-ヶー－]+\z/,
-                                         message: 'Full-width katakana characters' }
+                               format: { with: NAME_KANA_REGEX,
+                                         message: 'Full-width katakana characters'}
 
   validates :first_name_kana, presence: true,
-                              format: { with: /\A[ァ-ヶー－]+\z/,
-                                        message: 'Full-width katakana characters' }
+                              format: { with: NAME_KANA_REGEX,
+                                        message: 'Full-width katakana characters'}
 
   validates :birth_date, presence: true
 end
